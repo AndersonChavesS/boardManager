495a58c6c90954e4848526e8eeed4f8b3d9d89e7
Anderson Maicon Lemes De Souza Chaves <andersonchaves30@gmail.com>
2025-04-26 22:45:58 -0300

    chore: configuração inicial do projeto

---
+++ src/main/java/br/com/boardmanager/BoardmanagerApplication.java
@@ -0,0 +1,13 @@
+package br.com.boardmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardmanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/boardmanager/BoardmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.boardmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
